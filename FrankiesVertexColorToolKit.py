5e259d84e1882a19cc23e7943c261ad1583abac0
FrankieHobbins <acepainkiller@gmail.com>
2020-09-15 08:45:50 +0100

    added support for alpha and preset colour steps

--- FrankiesVertexColorToolKit.py
+++ FrankiesVertexColorToolKit.py
@@ -7,7 +7,7 @@ from mathutils import Vector, Color
 bl_info = {
     "name": "Frankies Vertex Colour Toolkit",
     "author": "Frankie",
-    "version": (0,2),
+    "version": (0,3),
     "blender": (2, 90, 0),
     "location": "View3D > Tool Panel",
     "description": "Lets you set vertex colours on objects or inside mesh",
@@ -17,6 +17,10 @@ bl_info = {
     "category": "3d View"}
 
 def interpolate(vector_a, vector_b, t):
+    print(vector_a)
+    print(vector_b)   
+    for i in range(vector_a):
+        print(i)
     lerp = ((1 - t) * vector_a + t * vector_b)
     return (lerp)
 
@@ -38,12 +42,12 @@ def calculateColourValue(color_original, color_new, gradient_start, gradient_pos
             if (bpy.context.scene.fvctk_randomiseMode=="0"):
                 return(random.random(), random.random(), random.random())
             if (bpy.context.scene.fvctk_randomiseMode=="1"):
-                return(random.random()*color_new[0], random.random()*color_new[1], random.random()*color_new[2])
+                return(random.random()*color_new[0], random.random()*color_new[1], random.random()*color_new[2],random.random()*color_new[3])
         if (bpy.context.scene.fvctk_randomiseScope=="1"):
             if (bpy.context.scene.fvctk_randomiseMode=="0"):
                 return(bpy.context.scene.fvctk_randomColor)
             if (bpy.context.scene.fvctk_randomiseMode=="1"):
-                return(bpy.context.scene.fvctk_randomColor[0]*color_new[0], bpy.context.scene.fvctk_randomColor[1]*color_new[1], bpy.context.scene.fvctk_randomColor[2]*color_new[2])
+                return(bpy.context.scene.fvctk_randomColor[0]*color_new[0], bpy.context.scene.fvctk_randomColor[1]*color_new[1], bpy.context.scene.fvctk_randomColor[2]*color_new[2],bpy.context.scene.fvctk_randomColor[3]*color_new[3])
 
     if (bpy.context.scene.fvctk_mode=="5"): #gradient
         return(interpolate(gradient_start, color_new, gradient_position))
@@ -130,6 +134,8 @@ def modifyVertexColor(color,current_mode):
                         me.vertex_colors[me.vertex_colors.active_index].data[loop_idx].color[1] = c[1]
                     if (bpy.context.scene.fvctk_bBool == True):
                         me.vertex_colors[me.vertex_colors.active_index].data[loop_idx].color[2] = c[2]
+                    if (bpy.context.scene.fvctk_aBool == True):
+                        me.vertex_colors[me.vertex_colors.active_index].data[loop_idx].color[3] = c[3]
 
     else:    
         for face in me.polygons:
@@ -152,11 +158,13 @@ def modifyVertexColor(color,current_mode):
                         me.vertex_colors[me.vertex_colors.active_index].data[loop_idx].color[1] = c[1]
                     if (bpy.context.scene.fvctk_bBool == True):
                         me.vertex_colors[me.vertex_colors.active_index].data[loop_idx].color[2] = c[2]
+                    # TODO: add alpha selection here
+                    me.vertex_colors[me.vertex_colors.active_index].data[loop_idx].color[3] = c[3]
 
         bpy.ops.object.mode_set(mode='EDIT', toggle=True)
     return()
 
-def button(mode):
+def button(mode, value):
     activeObject = bpy.context.active_object
     for ob in bpy.context.selected_objects:
         if (ob.type=="MESH"):
@@ -167,7 +175,7 @@ def button(mode):
 
             current_mode = bpy.context.object.mode
             bpy.context.view_layer.objects.active = ob
-            bpy.context.scene.fvctk_randomColor = random.random(), random.random(), random.random()
+            bpy.context.scene.fvctk_randomColor = random.random(), random.random(), random.random(), random.random()
 
             if (mode == 0): #from colour picker
                 bpy.context.object.update_from_editmode() #maybe dont need this
@@ -188,8 +196,18 @@ def button(mode):
                 newB = bpy.context.scene.fvctk_b
                 if (newB > 1):
                     newB /= 255
+                newA = bpy.context.scene.fvctk_a
+                if (newA > 1):
+                    newA /= 255
                 bpy.ops.object.mode_set(mode='EDIT')
-                modifyVertexColor(Color((newR, newG, newB)),current_mode)
+                modifyVertexColor([newR, newG, newB, newA],current_mode)
+
+            if (mode == 2):
+                bpy.context.object.update_from_editmode() #maybe dont need this
+                current_mode = bpy.context.object.mode
+                bpy.ops.object.mode_set(mode='EDIT')
+                value = value / 255
+                modifyVertexColor([value, value, value, value], current_mode)
 
             bpy.ops.object.mode_set(mode=current_mode)
     bpy.context.view_layer.objects.active = activeObject
@@ -207,22 +225,25 @@ class FVCTK(bpy.types.Panel):
         return context.mode in {'OBJECT', 'EDIT_MESH'}
 
     bpy.types.Scene.fvctk_selection = bpy.props.BoolProperty(name="Selected verts only", default=True)
-    bpy.types.Scene.fvctk_world = bpy.props.BoolProperty(name="World space", default=False  , description="Operate on verts in world space or local space")
+    bpy.types.Scene.fvctk_world = bpy.props.BoolProperty(name="World space", default=False, description="Operate on verts in world space or local space")
     bpy.types.Scene.fvctk_r = bpy.props.FloatProperty(name="R")
     bpy.types.Scene.fvctk_rBool = bpy.props.BoolProperty(name="R", default=True)
     bpy.types.Scene.fvctk_g = bpy.props.FloatProperty(name="G")
     bpy.types.Scene.fvctk_gBool = bpy.props.BoolProperty(name="G", default=True)
     bpy.types.Scene.fvctk_b = bpy.props.FloatProperty(name="B")
-    bpy.types.Scene.fvctk_bBool = bpy.props.BoolProperty(name="B", default=True)    
+    bpy.types.Scene.fvctk_bBool = bpy.props.BoolProperty(name="B", default=True)
+    bpy.types.Scene.fvctk_a = bpy.props.FloatProperty(name="A")
+    bpy.types.Scene.fvctk_aBool = bpy.props.BoolProperty(name="A", default=True)
     bpy.types.Scene.fvctk_mode = bpy.props.EnumProperty(name="Mode", items=[("0", "Replace", ""), ("1", "Add", ""),("2", "Subtract", ""), ("3", "Multiply", ""), ("4", "Randomise", ""), ("5", "Gradient", "")], default='0',)
     bpy.types.Scene.fvctk_gradient = bpy.props.EnumProperty(name="Gradient Axis", items=[("0", "X", ""), ("1", "Y", ""),("2", "Z", "")], default='2',)
     bpy.types.Scene.fvctk_gradientTop = bpy.props.EnumProperty(name="GradientTop", items=[("0", "Highest Vert", ""), ("1", "3d Cursor", ""),("2", "VCT", "Uses location of any object with the name VCT")], default='0',)
     bpy.types.Scene.fvctk_gradientBottom = bpy.props.EnumProperty(name="GradientBottom", items=[("0", "Lowest Vert", ""),("1", "0,0,0", ""), ("2", "3d Cursor", ""),("3", "VCB", "Uses location of any object with the name VCB")], default='1',)
     bpy.types.Scene.fvctk_randomiseScope = bpy.props.EnumProperty(name="RandomiseScope", items=[("0", "Verts", ""), ("1", "Objects", "")], default='0',)
     bpy.types.Scene.fvctk_randomiseMode = bpy.props.EnumProperty(name="RandomiseMode", items=[("0", "Replace", ""), ("1", "Multipty", "")], default='0',)
-    bpy.types.Scene.fvctk_picker = bpy.props.FloatVectorProperty(subtype='COLOR_GAMMA', default=(1,1,1), min=0.0, max=1.0, description="Color Picker")
-    bpy.types.Scene.fvctk_pickerGradientStart = bpy.props.FloatVectorProperty(subtype='COLOR_GAMMA', default=(0,0,0), min=0.0, max=1.0, description="Color Picker")
-    bpy.types.Scene.fvctk_randomColor = bpy.props.FloatVectorProperty(subtype='COLOR_GAMMA', default=(1,1,1), min=0.0, max=1.0, description="Color Picker")
+    bpy.types.Scene.fvctk_picker = bpy.props.FloatVectorProperty(subtype='COLOR_GAMMA', size=4, default=(1,1,1,0), min=0.0, max=1.0, description="Color Picker")
+    bpy.types.Scene.fvctk_pickerGradientStart = bpy.props.FloatVectorProperty(subtype='COLOR_GAMMA', size=4, default=(0,0,0,0), min=0.0, max=1.0, description="Color Picker")
+    bpy.types.Scene.fvctk_randomColor = bpy.props.FloatVectorProperty(subtype='COLOR_GAMMA', size=4, default=(1,1,1,0), min=0.0, max=1.0, description="Color Picker")
+    bpy.types.Scene.fvctk_numberOfColours = bpy.props.IntProperty(default=1, name="Number Of Colours", min=0,max=255)
 
     def draw(self, context):
         layout = self.layout
@@ -245,6 +266,8 @@ class FVCTK(bpy.types.Panel):
         row.prop(context.scene, "fvctk_gBool")
         row.prop(context.scene, "fvctk_bBool")
         row = layout.row()
+        row.prop(context.scene, "fvctk_aBool")
+        row = layout.row()
 
         layout.separator()
         layout.label(text="Assign Vertex Colour From Values:")
@@ -253,6 +276,7 @@ class FVCTK(bpy.types.Panel):
         row.prop(context.scene, "fvctk_r")
         row.prop(context.scene, "fvctk_g")
         row.prop(context.scene, "fvctk_b")
+        row.prop(context.scene, "fvctk_a")
         row = layout.row()
         row.prop(context.scene, "fvctk_mode")
 
@@ -313,13 +337,25 @@ class FVCTK(bpy.types.Panel):
             #row = layout.row()
             #row.prop(context.scene, "fvctk_randomiseMode",expand=True)
 
+        row = layout.row()
+        row.scale_y = 1.5
+        row.prop(context.scene, "fvctk_numberOfColours")    
+        for i in range(bpy.context.scene.fvctk_numberOfColours):
+            row = layout.row()
+            value = (255/bpy.context.scene.fvctk_numberOfColours)*i
+            value += (0.5 * (255/bpy.context.scene.fvctk_numberOfColours))
+            value = round(value)
+            props = row.operator("vertex_col.apply",text=str(value), icon="VPAINT_HLT")
+            props.mode = 2
+            props.value = value
+
 class ApplyVertColPicker(bpy.types.Operator):
     bl_idname = "vertex_col.apply_picker"
     bl_label = "Assign"
     bl_description = "Assign color to selected vertices for selected vertex color layer"
 
     def execute(self, context):
-        button(0)
+        button(mode=0,value=0)
         return{'FINISHED'}
 
 class ApplyVertColRGB(bpy.types.Operator):
@@ -328,18 +364,40 @@ class ApplyVertColRGB(bpy.types.Operator):
     bl_description = "Assign color to selected vertices for selected vertex color layer"
 
     def execute(self, context):
-        button(1)
-        return{'FINISHED'}
+        button(mode=1,value=0)
+        return{'FINISHED'}    
+
+class ApplyVertCol(bpy.types.Operator):
+    bl_idname = "vertex_col.apply"
+    bl_label = "Assign"
+    bl_description = "Assign color to selected vertices for selected vertex color layer"
+    
+    mode: bpy.props.IntProperty(
+        name="mode",
+        default=0
+    )
+    
+    value: bpy.props.IntProperty(
+        name="value",
+        default=0
+    )
+
+    def execute(self, context):
+        button(self.mode,self.value)
+        return{'FINISHED'}    
+
 
 def register():
     bpy.utils.register_class(FVCTK)
     bpy.utils.register_class(ApplyVertColPicker)
     bpy.utils.register_class(ApplyVertColRGB)
+    bpy.utils.register_class(ApplyVertCol)
 
 def unregister():
     bpy.utils.unregister_class(FVCTK)
     bpy.utils.unregister_class(ApplyVertColPicker)
     bpy.utils.unregister_class(ApplyVertColRGB)
+    bpy.utils.unregister_class(ApplyVertCol)
 
 if __name__ == "__main__":
     register()
